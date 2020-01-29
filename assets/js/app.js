934efe2689f71ceb7961576a6ebec51aa78d8722
Mark Younan <markyounan@gmail.com>
2020-01-29 10:58:11 -0800

    styling and function setup

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+console.log(questions);
+
+/*
+  on page load: 
+    show game start screen
+    include how to play instructions
+    add start quiz button
+    
+  on start button click:
+    hide game-start div 
+    show questions div
+    show question
+    start timer
+  on answer selection:
+    check to see if answer is correct
+    if correct:
+      display success message
+      add to score
+      show next question
+    if incorrect:
+      display fail message
+      deduct time from timer
+      show next question
+  for game end:
+    if all the questions have been answered:
+      display score
+      prompt to save initials and results
+      on save:
+        record results in local storage
+    if time runs out:
+      display score
+      prompt to try again
+*/
+
+// functions:
+
+function startGame(){
+
+}
+
+function resetGame(){
+
+}
+
+function endGame(){
+
+}
+
+function restartGame(){
+
+}
+
+function startTimer(){
+
+}
+
+function decrementTimer(){
+
+}
+
+function renderQuestion(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function saveScore(){
+
+}
+
+function clearScores(){
+
+}
+
+function toggleScores(){
+
+}
